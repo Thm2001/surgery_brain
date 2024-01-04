a78b1c2b49055a1f96f4ae63053812ac4f97d953
yueht23 <yueht17@gmail.com>
2024-01-04 10:51:34 +0000

    delete the useless package import due to the Django version problem

--- surgery_brain/urls.py
+++ surgery_brain/urls.py
@@ -1,9 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
-from django.views import static
-from django.conf import settings
-from django.conf.urls import url
 
 urlpatterns = [
 
